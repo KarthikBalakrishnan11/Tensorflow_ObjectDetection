04f502853e3b98a70a6d8cf97a06d177c28313e8
Karthik B <39647840+KarthikBalakrishnan11@users.noreply.github.com>
2019-06-23 15:01:01 +0530

    Add files via upload

---
+++ object_detection_video.py
@@ -0,0 +1,175 @@
+import numpy as np
+import os
+#import sys
+import tensorflow as tf
+import cv2
+
+#sys.path.append('/home/karthik/Karthik/TF/models/research/')
+#sys.path.append('/home/karthik/Karthik/TF/models/research/object_detection/utils')
+#sys.path.append('/home/karthik/Karthik/TF/models/research/slim')
+
+from distutils.version import StrictVersion
+
+# This is needed since the notebook is stored in the object_detection folder.
+from object_detection.utils import ops as utils_ops
+
+if StrictVersion(tf.__version__) < StrictVersion('1.12.0'):
+  raise ImportError('Please upgrade your TensorFlow installation to v1.12.*.')
+
+"""## Env setup"""
+
+# This is needed to display the images.
+# %matplotlib inline
+
+"""## Object detection imports
+Here are the imports from the object detection module.
+"""
+
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+
+# What model to download.
+MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
+
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
+PATH_TO_FROZEN_GRAPH = MODEL_NAME + '/frozen_inference_graph.pb'
+
+# List of the strings that is used to add correct label for each box.
+PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
+
+"""## Load a (frozen) Tensorflow model into memory."""
+
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+  od_graph_def = tf.GraphDef()
+  with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH, 'rb') as fid:
+    serialized_graph = fid.read()
+    od_graph_def.ParseFromString(serialized_graph)
+    tf.import_graph_def(od_graph_def, name='')
+
+"""## Loading label map
+Label maps map indices to category names, so that when our convolution network predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
+"""
+category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+
+"""# Detection"""
+# If you want to test the code with your images, just add path to the images to the TEST_IMAGE_PATHS.
+#PATH_TO_TEST_IMAGES_DIR = '/home/karthik/Karthik/TF/models/research/object_detection/test_images'
+#TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image{}.jpg'.format(i)) for i in range(1, 4) ]
+
+
+def run_inference_for_single_image(image, graph):
+  with graph.as_default():
+    with tf.Session() as sess:
+      # Get handles to input and output tensors
+      ops = tf.get_default_graph().get_operations()
+      all_tensor_names = {output.name for op in ops for output in op.outputs}
+      tensor_dict = {}
+      for key in [
+          'num_detections', 'detection_boxes', 'detection_scores',
+          'detection_classes', 'detection_masks'
+      ]:
+        tensor_name = key + ':0'
+        if tensor_name in all_tensor_names:
+          tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
+              tensor_name)
+      if 'detection_masks' in tensor_dict:
+        # The following processing is only for single image
+        detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
+        detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
+        # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
+        real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
+        detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
+        detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
+        detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
+            detection_masks, detection_boxes, image.shape[1], image.shape[2])
+        detection_masks_reframed = tf.cast(
+            tf.greater(detection_masks_reframed, 0.5), tf.uint8)
+        # Follow the convention by adding back the batch dimension
+        tensor_dict['detection_masks'] = tf.expand_dims(
+            detection_masks_reframed, 0)
+      image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+
+      # Run inference
+      output_dict = sess.run(tensor_dict,
+                             feed_dict={image_tensor: image})
+
+      # all outputs are float32 numpy arrays, so convert types as appropriate
+      output_dict['num_detections'] = int(output_dict['num_detections'][0])
+      output_dict['detection_classes'] = output_dict[
+          'detection_classes'][0].astype(np.int64)
+      output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
+      output_dict['detection_scores'] = output_dict['detection_scores'][0]
+      if 'detection_masks' in output_dict:
+        output_dict['detection_masks'] = output_dict['detection_masks'][0]
+        
+    #print ("Output",output_dict)
+    return output_dict
+
+
+cap = cv2.VideoCapture(0)
+
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
+out = cv2.VideoWriter('test_images/output.avi',cv2.VideoWriter_fourcc('X','V','I','D'), 10, (frame_width,frame_height))
+
+skip=1
+
+while(True):
+    ret, image = cap.read() 
+    skip+=1
+    if (ret==False):     
+        break
+    if (skip%10==0):
+        
+        #image=cv2.imread(image_path)
+        #image=cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+        scale_percent = 60 # percent of original size
+        width = int(image.shape[1] * scale_percent / 100)
+        height = int(image.shape[0] * scale_percent / 100)
+        dim = (width, height)
+        resized_image = cv2.resize(image,dim, interpolation = cv2.INTER_AREA)
+        image_np_expanded = np.expand_dims(resized_image, axis=0)
+        # Actual detection.
+        output_dict = run_inference_for_single_image(image_np_expanded, detection_graph)
+        # Visualization of the results of a detection.
+       
+        output_classes=output_dict['detection_classes']
+        
+        get_indexes = lambda output_classes, xs: [i for (y, i) in zip(xs, range(len(xs))) if output_classes == y]
+        indexes=get_indexes(1,output_classes)
+        #print("Indexes",indexes)
+        detection_boxes=[]
+        detection_classes=[]
+        detection_scores=[]
+        
+        for index in indexes:
+            detection_boxes.append(output_dict['detection_boxes'][index])
+            detection_classes.append(output_dict['detection_classes'][index])
+            detection_scores.append(output_dict['detection_scores'][index])
+        
+        detection_boxes=np.asarray(detection_boxes)    
+        detection_classes=np.asarray(detection_classes)    
+        detection_scores=np.asarray(detection_scores)
+        
+        print("Scores:",detection_scores)
+        
+        #print(type(output_dict['detection_boxes']))
+        
+        vis_util.visualize_boxes_and_labels_on_image_array(
+              resized_image,
+              detection_boxes,
+              detection_classes,
+              detection_scores,
+              category_index,
+              instance_masks=output_dict.get('detection_masks'),
+              use_normalized_coordinates=True,
+              line_thickness=8)
+        out.write(resized_image)
+      
+        cv2.imshow("Output",resized_image)
+        cv2.waitKey(1)
+  
+cap.release()
+out.release()
+cv2.destroyAllWindows()
